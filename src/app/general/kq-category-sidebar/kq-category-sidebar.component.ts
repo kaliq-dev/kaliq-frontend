0d65cdbc2b9f1d11e906da9275827f45cdcf42e9
abrarShariar <shariarabrar@gmail.com>
2017-07-01 16:06:29 +0600

    July 01 update

--- src/app/general/kq-category-sidebar/kq-category-sidebar.component.ts
+++ src/app/general/kq-category-sidebar/kq-category-sidebar.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-kq-category-sidebar',
@@ -8,19 +9,19 @@ import {Component, OnInit} from '@angular/core';
 export class KqCategorySidebarComponent implements OnInit {
 
   public categoryList = [
-    {name: 'Hardware', img: 'HARDWARE.png'},
-    {name: 'Office Supplies', img: 'OFFICE_SUPPLIES.png'},
-    {name: 'Power Tools', img: 'POWER_TOOL.png'},
-    {name: 'Security', img: 'SECURITY.png'},
-    {name: 'Paint', img: 'PAINT.png'},
-    {name: 'Nuts & Bolts', img: 'NUTS_BOLTS.png'},
-    {name: 'Lighting', img: 'LIGHTING.png'},
-    {name: 'Electronics', img: 'ELECTRONICS.png'},
-    {name: 'Electrical', img: 'ELECTRICALS.png'},
-    {name: 'Equipment & Mac', img: 'EQUIPMENTS_MECHNERIES.png'},
-    {name: 'Car', img: 'HARDWARE.png'},
-    {name: 'Coffee', img: 'HARDWARE.png'},
-    {name: 'Computers', img: 'HARDWARE.png'}
+    {name: 'Hardware', img: 'HARDWARE.png', hover_img: 'HARDWARE[on].png' },
+    {name: 'Office Supplies', img: 'OFFICE_SUPPLIES.png', hover_img: 'OFFICE_SUPPLIES[on].png'},
+    {name: 'Power Tools', img: 'POWER_TOOL.png', hover_img: 'POWER_TOOL[on].png'},
+    {name: 'Security', img: 'SECURITY.png',hover_img: 'SECURITY[on].png'},
+    {name: 'Paint', img: 'PAINT.png',hover_img: 'PAINT[on].png'},
+    {name: 'Nuts & Bolts', img: 'NUTS_BOLTS.png',hover_img: 'NUTS_BOLTS[on].png'},
+    {name: 'Lighting', img: 'LIGHTING.png',hover_img: 'LIGHTING[on].png'},
+    {name: 'Electronics', img: 'ELECTRONICS.png' , hover_img: 'ELECTRONICS[on].png'},
+    {name: 'Electrical', img: 'ELECTRICALS.png', hover_img: 'ELECTRICALS[on].png'},
+    {name: 'Equipment & Mac', img: 'EQUIPMENTS_MECHNERIES.png', hover_img: 'EQUIPMENTS_MECHNERIES[on].png'},
+    {name: 'Car', img: 'HARDWARE.png', hover_img: 'HARDWARE[on].png'},
+    {name: 'Coffee', img: 'HARDWARE.png', hover_img: 'HARDWARE[on].png'},
+    {name: 'Computers', img: 'HARDWARE.png', hover_img: 'HARDWARE[on].png'}
   ];
 
   constructor() {
@@ -30,5 +31,13 @@ export class KqCategorySidebarComponent implements OnInit {
 
   }
 
+  mouseEnterCategoryItem(category){
+    category['temp'] = category['img'];
+    category['img'] = category['hover_img'];
+  }
+
+  mouseLeaveCategoryItem(category){
+    category['img'] = category['temp'];
+  }
 
 }
